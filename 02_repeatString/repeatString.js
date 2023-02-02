425262fde51b8d56bb199f8a876eb54659a4ee8d
Fianu Dovlo <dovlofianu@gmail.com>
2023-02-02 22:42:35 +0000

    Feature: Passed initial test Repeat the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let heys = "";
+    for (i = 1; i <= num;  i++) {
+        heys += string;
+    }
+    return heys;
 };
 
 // Do not edit below this line
