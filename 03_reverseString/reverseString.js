328849ad1cb6f51837fc9ad89dfc835e45bdfe2a
Fianu Dovlo <dovlofianu@gmail.com>
2023-02-06 21:59:21 +0000

    Reversed string using built-in functions split(), reverse() and join() Then returned the output string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Step One: Use the split() method to return a new array
+    let splitString = string.split("");
+
+    // Step Two: Use reverse() method to reverse the new array
+    let reverseArray = splitString.reverse();
+
+    // Step Three: Use the join() mthod to join all elements of the array into a string
+    let joinArray = reverseArray.join();
+
+    //Step Four: Return the reversed String
+    return joinArray;
 
 };
 
