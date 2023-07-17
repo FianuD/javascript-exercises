24a4de445441bf5b3c89387958f0b0dcd5385a9e
Fianu Dovlo <dovlofianu@gmail.com>
2023-07-17 21:25:55 +0000

    fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    let figureOne = 0;
+    let figureTwo = 1;
+    let fibonacciNumber;
 
+    if(num == 1 || num == 2) return figureTwo;
+    
+    for(let i=1; i < num; ++i) {
+        fibonacciNumber = figureOne + figureTwo;
+        figureOne = figureTwo;
+        figureTwo = fibonacciNumber
+    }
+    return fibonacciNumber;
 };
 
 // Do not edit below this line
