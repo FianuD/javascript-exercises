5012d348b21bbad622f6c5f5bd8b41c6ed39d94c
Fianu Dovlo <dovlofianu@gmail.com>
2023-07-17 21:14:09 +0000

    Palindromes problem solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    // [A-Za-z] is a regular expression that will match anything between A-Z and a-z.
+    // ^ before indacates anything that is NOT an alphabet, should be replaced with "". An empty string.
 
+    let reverseString = (
+        processedString.split("").reverse().join("")
+        // split the string into an array.
+        // reverse the content of the array.
+        // join the array back into a string.
+    );
+    return processedString == reverseString;
+    // if processedString is the same as the reverseString then the string is a palindrome.
 };
 
 // Do not edit below this line
